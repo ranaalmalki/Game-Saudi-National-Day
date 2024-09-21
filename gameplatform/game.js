e93a5e6fd68ce515433d07d9a0032f36b9cfdb88
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-21 21:53:30 +0300

    make the function for cham]ge the qustion & add score to the user

--- gameplatform/game.js
+++ gameplatform/game.js
@@ -3,3 +3,116 @@ let namee=document.getElementById("show-name")
 let username = localStorage.getItem('username')
 
 namee.textContent=username
+
+
+
+
+    // let question_content =document.getElementById("qus-content")
+
+// let div_row = document.createElement("div")
+// div_row.setAttribute("class","row justify-content-around card-body")
+// div_row.setAttribute("class","row")
+// question.setAttribute("class","text-center")
+
+//     let div_row1 = document.createElement("div")
+//     div_row.setAttribute("class","row justify-content-around card-body")
+//     let button_1=document.createElement("button")
+//     button_1.setAttribute("class","col-4 border btn border-dark")
+//     // let button_2=document.createElement("button")
+//     // button_1.setAttribute("class","col-4 border btn border-dark")
+//     // let div_row2 = document.createElement("div")
+//     // div_row.setAttribute("class","row justify-content-around card-body")
+     
+// question_content.appendChild(div_row)
+// question_content.appendChild(div_row1)
+// div_row.appendChild(question_1)
+// div_row1.appendChild(button_1)
+
+//     question.map(q=>{
+//         question_1.textContent=q.qus
+//         // button_1.textContent=q.opstion
+
+
+// })
+let question_1 =document.getElementById("qustion-1")
+
+
+let score_player=document.getElementById("score-player");
+let btn_1=document.getElementById("btn-1");
+let btn_2=document.getElementById("btn-2");
+let btn_3=document.getElementById("btn-3");
+let btn_4=document.getElementById("btn-4");
+
+let question =[
+    {
+       qus:" متى تم توحيد المملكة العربية السعودية يوم؟",
+       opstion:["23 سبتمبر","24 سبتمبر","25 سبتمير","22 سبتمبر"],
+       answer:0,
+   },
+   {
+       qus:"ما هي عاصمة السعودية؟",
+       opstion:["الرياض", "جدة", "مكة", "الدمام"],
+       answer:0,
+   },
+   {
+       qus:"ما هو اللون الوطني للمملكة؟",
+       opstion:["الأخضر", "الأحمر", "الأزرق", "الأصفر"],
+       answer:0,
+   },
+   ]
+   
+
+let currentqu = 0;
+function nextQustion(){
+    let currentqution = question[currentqu];
+    question_1.textContent=currentqution.qus
+    btn_1.textContent = currentqution.opstion[0];
+    btn_2.textContent = currentqution.opstion[1];
+    btn_3.textContent = currentqution.opstion[2];
+    btn_4.textContent = currentqution.opstion[3];
+    answerd = false; 
+}
+
+
+let score = 0;
+let answerd= false;
+function answerCorrect(isCorrect){
+    if(!answerd){
+        answerd=true
+    if(isCorrect){
+        score+=100;
+score_player.textContent=`النقاط:${score}`
+alert("good")
+    }else{
+        alert("غجاية خاطئة")
+    }
+
+
+setTimeout(()=>{
+    currentqu++;
+    if(currentqu < question.length){
+        nextQustion()
+    }else{
+        alert("finish")
+        window.location.href=""
+    }
+   
+} , 1000)
+    }
+}
+
+
+btn_1.addEventListener("click",()=>{
+    answerCorrect(question[currentqu].answer===0)
+})
+btn_2.addEventListener("click",()=>{
+    answerCorrect(question[currentqu].answer===1)
+})
+btn_3.addEventListener("click",()=>{
+    answerCorrect(question[currentqu].answer===2)
+})
+btn_4.addEventListener("click",()=>{
+    answerCorrect(question[currentqu].answer===3)
+})
+
+nextQustion()
--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 let url="https://66e7e69bb17821a9d9da6eb2.mockapi.io/login"
-let fname = document.getElementById("input-name")
-let form_id=document.getElementById("form-id")
+let form_id=document.getElementById("add-name")
+
+// players.map(e=>{
+// if(e.length >= 3){
+//     console.log('stop');
+    
+// }else if(e.length <=3){
+//     players.push(fname.value)
+// }
+//     console.log(e);
+//     // console.log(players);
+
+// //     if(e.length==3)
+// // console.log("is 3 player");
+
+// })
+console.log(players);
+
+  
+
+// })
+
+
+
 form_id.addEventListener("submit", e=>{
+    let fname = document.getElementById("input-name")
+
     e.preventDefault()
 
 fetch(url,{
@@ -15,8 +39,35 @@ fetch(url,{
 })
 .then((response) => response.json())
 .then(json => {
-    if(json){
+    let players= []
+
+    if(players.length ===3){
+        console.log("sart the game");
+    
+    }else{
+        players.push(fname.value)
+        console.log(players.length);
+        console.log(players);
+    
+    }
         localStorage.setItem('username', fname.value)
-window.location.href='./gameplatform/gamepage.html'}})
+window.location.href='./gameplatform/gamepage.html'})
 
 })
+
+// form_id.addEventListener("click",()=>{
+//     // let fname = document.getElementById("input-name")
+
+  
+
+
+// // if(players.length >=3){
+// //     console.log("stop");
+
+// // }else{
+// //     players.push(fname.value)
+// //     console.log(players.length);
+// //     console.log(players);
+
+// // }
+// })
